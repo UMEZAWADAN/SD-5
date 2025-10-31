14d7ca954aba188877230c38488dbbade7264b06
UMEZAWADAN <c4p31017@bunkyo.ac.jp>
2025-11-01 00:24:05 +0900

    変更

--- route.py
+++ route.py
@@ -1,3 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# 詳細ページのルート
 @app.route("/shousai")
 def shousai():
     return render_template("shousai.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
