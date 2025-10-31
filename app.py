7bd8749b2d4b34b74281f7b0094e2948cea68e70
UMEZAWADAN <c4p31017@bunkyo.ac.jp>
2025-11-01 00:34:42 +0900

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# 詳細ページのルート
+@app.route("/")
+def index():
+    return "<p>動作確認用トップ。<a href='/shousai'>対象者詳細へ</a></p>"
+
 @app.route("/shousai")
 def shousai():
     return render_template("shousai.html")
