cf53a72c0babc43ed5f3293c5bb428780d050040
c4p31010 <c4p31010@bunkyo.ac.jp>
2025-11-09 01:32:01 +0900

    利用者基本情報変更（途中）

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def list():
 def shousai():
     return render_template("shousai.html")
 
+#後で削除して
+@app.route("/shousai_kihon")
+def shousai_kihon():
+    return render_template("shousai_kihon.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
